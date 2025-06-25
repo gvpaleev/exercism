082eea5f0a752c799e726ccb8a500e522bdf21c5
Gennady Paleev <gv.paleev@gmail.com>
2025-06-25 18:07:11 +0300

    adder

---
+++ adder/target/debug/deps/adder-13f47c2e7370e772.d
@@ -0,0 +1,7 @@
+/home/genus/exercism/rust/adder/target/debug/deps/libadder-13f47c2e7370e772.rmeta: src/lib.rs
+
+/home/genus/exercism/rust/adder/target/debug/deps/libadder-13f47c2e7370e772.rlib: src/lib.rs
+
+/home/genus/exercism/rust/adder/target/debug/deps/adder-13f47c2e7370e772.d: src/lib.rs
+
+src/lib.rs:
---
+++ adder/target/debug/deps/adder-2b9442a7527ba17b.d
@@ -0,0 +1,5 @@
+/home/genus/exercism/rust/adder/target/debug/deps/adder-2b9442a7527ba17b: src/lib.rs
+
+/home/genus/exercism/rust/adder/target/debug/deps/adder-2b9442a7527ba17b.d: src/lib.rs
+
+src/lib.rs:
